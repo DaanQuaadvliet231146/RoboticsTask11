940b0746aa80b94187dff34087f627b22a85c393
DaanQuaadvliet231146 <231146@buas.nl>
2025-01-15 09:05:39 +0100

    Update task11_final_2.py

--- task11_final_2.py
+++ task11_final_2.py
@@ -13,7 +13,7 @@ from ot2_gym_wrapper_V2_Reward3 import OT2Env
 
 # Use the appropriate project name and task name (if you are in the first group in Dean's mentor group, use the project name 'Mentor Group D/Group 1')
 # It can also be helpful to include the hyperparameters in the task name
-task = Task.init(project_name='Mentor Group K/Group 1/DaanQuaadvliet', task_name='DaanQuaadvliet_Final_model_Iteration2_Reward2')
+task = Task.init(project_name='Mentor Group K/Group 1/DaanQuaadvliet', task_name='DaanQuaadvliet_Final_model_Iteration2_Reward3')
 #copy these lines exactly as they are
 #setting the base docker image
 wrapped_env = OT2Env()
